27bf5c12bfb3ee2e43015ad9ae1666d1feac00dc
David D. Riddle <ddriddle@illinois.edu>
2015-07-09 12:13:19 -0500

    Tutorial 4: Adding endpoints for our User models
    modified:   snippets/serializers.py
    modified:   snippets/views.py
    modified:   tutorial/urls.py
    
    http://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#adding-endpoints-for-our-user-models

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,17 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
